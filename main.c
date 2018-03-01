087dfa379cc1bfaea80b170e974147692ea9d0a9
Loic Strf <loic.strafella@gmail.com>
2018-03-01 11:32:45 +0100

    Changing platform
    working on Forest.h. Project do not compile, give me few minutes

--- main.c
+++ main.c
@@ -29,19 +29,7 @@
 
 #include <SDL/SDL.h>
 
-/* structure for moultiple linked list */
-typedef struct Node Node;
-struct Node{
-    int n_subnode;
-    float value;
-    Node **next;
-};
-
-/* One tree for the planet */
-typedef struct Tree Tree;
-struct Tree{
-    Node *root;
-};
+/* include file Forest.h which is the header for Tree structure */
 
 
 /* Function prototype */
@@ -220,7 +208,7 @@ int main ( int argc, char** argv ){
     } // end main loop
 
     // all is well ;
-    printf("Exited cleanly\n");
+    printf("Exited SDL cleanly\n\n");
 
     /* SDL free allocated memory */
     SDL_FreeSurface(white_cell);
@@ -296,79 +284,107 @@ void test_4_winner(int **board, int *any_winner, char *win_sign){
  *
  */
 Tree * initialize_tree(){
-    int i, j,k, n_lvl;
-    int *n_nodes_to_alloc;
+
+    int i, j, k, n_lvl, current_n_leaf, next_n_leaf;
+    int leaf_by_lvl[NCELL*NCELL];
 
     Tree *tree;
-    Node *first, *current;
+    Node *current, *first;
 
-    n_lvl = NCELL*NCELL -1;
-    n_nodes_to_alloc = (int) malloc(n_lvl*sizeof(int));
+    /* number of level of the tree */
+    n_lvl = NCELL*NCELL;
 
-    /* number of nodes to alloc per level */
-    n_nodes_to_alloc[0] = n_lvl;
-    for(i=1; i<n_lvl; i++){
-        n_lvl --;
-        n_nodes_to_alloc[i] = n_nodes_to_alloc[i-1]*n_lvl;
-    }
+    /* allocate the tree =) */
+    tree = (Tree *) malloc(sizeof(Tree *));
+    if( !tree )
+        printf("Allocation failed !");
+    else
+        printf("Allocation of Tree success - mem adresse %p\n", tree);
 
-    /* Feed the tree =) */
-    tree = malloc(sizeof(Tree *));
-    first = malloc(sizeof(Node *));
+    /* compute leaf by level */
+    leaf_by_lvl[0] = 1;
+    for(i=1; i<n_lvl; i++)
+        leaf_by_lvl[i] = n_lvl-i;
 
+    #ifdef DEBUG
+    for(i=0; i<n_lvl; i++)
+        printf("---> lvl %d -> n_leaf = %d\n", i, leaf_by_lvl[i]);
+    printf("\n");
+    #endif
+
+    /* allocate the first node of the tree, ie : root */
+    current_n_leaf = NCELL*NCELL-1;
+    first = (Node *) malloc(sizeof(Node *));
+    //first->n_subnode = current_n_leaf;
+
+    //first->next = (Node **) malloc(first->n_subnode * sizeof(Node *));
+
+    first->previous = NULL;
     tree->root = first;
-    first->next = (Node **) malloc(n_nodes_to_alloc[0]*sizeof(Node *));
-    first->n_subnode = n_nodes_to_alloc[0];
-    first->value = 0.0f;
 
-    for(i=0; i<first->n_subnode; i++)
-        first->next[i] = (Node *) malloc(sizeof(Node *));
+    #ifdef DEBUG
+    printf("|| -> Root at %p \n",tree->root);
+    #endif
 
+    /* start allocation of the whole tree */
     current = first;
-    Node *tmp;
-    for(i=1; i<(NCELL*NCELL)-1; i++){
-        /* alloc next lvl nodes */
-        tmp = (Node *) malloc(n_nodes_to_alloc[i]*sizeof(Node *));
+    n_lvl = 2;//NCELL*NCELL;
+    next_n_leaf = current_n_leaf -1;
+    for(i=0; i<n_lvl-1; i++){
+        printf("Level i=%d, n_leaf = %d\n",i, current_n_leaf);
+        for(j=0; j<leaf_by_lvl[i]; j++){
+            current->n_subnode = leaf_by_lvl[i+1];
+            current->next = (Node **) malloc((leaf_by_lvl[i+1])*sizeof(Node **));
+            for(k=0; k<leaf_by_lvl[i+1]; k++){
+                Node *ptr_tmp=NULL;
 
-        /* dispatch nodes */
-        int count = 0;
-        for(j=0; j<n_nodes_to_alloc[i-1]; j++){
-            current = current->next[j];
+                ptr_tmp = (Node *) malloc(sizeof(Node *));
+                if( !ptr_tmp )
+                    exit(EXIT_FAILURE);
 
-            for(k=0; k<n_nodes_to_alloc[i-1]-1; k++){
-                current->next[j] = &tmp[count];
-                count ++;
+                ptr_tmp->value = 0;
+                ptr_tmp->n_subnode = 0;
+                ptr_tmp->next = NULL;
+                ptr_tmp->previous = current;
+                current->next[k] =&ptr_tmp;
+
+                #ifdef DEBUG
+                printf("|||| -> Node %p as leaf %p \n",current, current->next[k]);
+                #endif
+                free(ptr_tmp);
             }
+            current = current->next[j];
         }
-
     }
 
-    /* the final the next node of the final node should be set to null */
-
-    /* free memory */
-    free(n_nodes_to_alloc);
+    /* the next pointor of all final node should be set to nul */
+    /* TODO but how ??? */
 
     return tree;
 }
-
-
 
 /*
  * Display the tree - used for debugging purpose !
  *
  */
 void show_tree(Tree *bonzai){
-    int i, j, count_lvl;
+    int i, j, count_lvl, count_leaf=0;
     Node *current;
 
-    count_lvl = 1;
+    count_lvl = 0;
     current = bonzai->root;
+    count_leaf = current->n_subnode;
 
     printf("\n");
     while(current->next[0] != NULL){
-        printf("Current node's level : %d , %d subnodes", count_lvl, current->n_subnode);
-        current = current->next[0];
-        count_lvl ++;
+        printf("-----> leaf : %p having % sub-leaf \n", current, current->n_subnode);
+
+        if( count_leaf == current->n_subnode){
+            count_leaf = 0;
+            current = current->next[count_leaf];
+        }else
+            count_leaf ++;
+
     }
 }
 
